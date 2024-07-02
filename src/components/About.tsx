55b551d26c12f798ebaaf2e6907bcf4e5d356647
Dami-18 <damiyagondha@gmail.com>
2024-07-02 20:50:35 +0530

    more changes hehe

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
         <div className='about' id='about'>
             <div className="img-container"><img src={myImg}></img></div>
             <div className="about-text"><p id='about-me'>I am Damiya Gondha, a sophomore pursuing computer science and engineering from IIT Kharagpur. 
-                <i>"Computer Science is an ocean I dive deep into!"</i> Yes, that holds true for me. I like to get myself immersed in the complexity of data structures, algorithms and software development. (<i>though sometimes DSA and bugs scare me ;-;</i>)
+                <i>"Computer Science is an ocean I dive deep into!"</i> Yes, that holds true for me. I like to get myself immersed in the complexity of data structures, algorithms and software development.
                 I am also interested in robotics and currently I am a part of software team at <a href='http://www.agv.iitkgp.ac.in/' target='_blank'>AGV</a>, IIT Kharagpur where I get to explore autonomous robotics. 
                 I started exploring open source by contributing to open source organization, <a href='https://github.com/metakgp' target='_blank'>MetaKGP</a>. Currently, I am a core team member at <a href='https://kossiitkgp.org/' target='_blank'>Kharagpur Open Source Society</a>, a bunch of cool open source enthusiasts. Now,
                 I have started believing in <i>"coding by humans, for humans"</i> and I find joy in contributing to projects that people use for various tasks.<br/>Apart from these, my interests include reading novels and music. I am an amateur flautist and love playing flute. I am passionate about teaching too and might think to become a professor one day:)</p></div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.scss'
 import 'slick-carousel/slick/slick-theme.scss'
+import '../styles/Projects.scss'
 
 const Projects = () => {
   const settings = {
@@ -15,13 +16,13 @@ const Projects = () => {
     <div className="project" id="projects">
       <Slider {...settings}>
             <div className="box-1 box">
-              <div className="proj-1"><p>Autumn 2023 Physics Sem Website</p></div>
+              <div className="proj-1"><p><a href="https://dami-18.github.io/Aut_2023_Physics/" target="_blank">Autumn 2023 Physics Sem Website</a></p></div>
               <div className="projtxt"><p>This website serves as a central hub for accessing study materials and resources tailored for the Physics semester during the first year at IIT Kharagpur. It contains drive links to well organized lecture notes, past year papers and books. A simple website to serve as an academic helper!</p></div>
               <div className="tech-stack"><p>Tech Stack: HTML, CSS</p></div>
               <div className="github"><a href="https://github.com/Dami-18/Aut_2023_Physics" target="_blank"><i className="fa-brands fa-github"></i></a></div>
             </div>
             <div className="box-2 box">
-              <div className="proj-2"><p>GYFE (Open source project maintained by <a href='https://github.com/metakgp' target='_blank'>MetaKGP</a>)</p></div>
+              <div className="proj-2"><p style={{fontWeight: "500"}}>GYFE (Open source project maintained by <a href='https://github.com/metakgp' target='_blank'>MetaKGP</a>)</p></div>
               <div className="projtxt"><p>Get Your Freaking Electives (GYFE) is a webapp for the students of IIT Kharagpur which simplifies the process of subject registration. It fetches data regarding available slots and elective subjects from their ERP student profile after providing credentials and provides a list of available electives.</p></div>
               <div className="tech-stack"><p>Tech Stack: React.js, TypeScript, SCSS, Flask</p></div>
               <div className="github"><a href="https://github.com/metakgp/gyfe" target="_blank"><i className="fa-brands fa-github"></i></a></div>
