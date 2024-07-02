c78a9ace8b614042410678ec9a2a8117bc8eef14
Dami-18 <damiyagondha@gmail.com>
2024-07-03 00:27:14 +0530

    add medium account link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
               <div><a href="https://www.instagram.com/damiyagondha/" target='_blank'><i className="fa-brands fa-instagram"></i></a></div>
               <div><a href="https://www.linkedin.com/in/damiya-gondha79/" target='_blank'><i className="fa-brands fa-linkedin-in"></i></a></div>
               <div><a href="https://github.com/Dami-18" target='_blank'><i className="fa-brands fa-github"></i></a></div>
-              <div><a href="mailto:damiyagondha@gmail.com" target="_blank"><i className="fa-regular fa-envelope"></i></a></div>
+              <div><a href="https://medium.com/@damiyagondha" target="_blank"><i className="fa-brands fa-medium"></i></a></div>
             </div>
         </div>
      );
